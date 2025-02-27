65501cbfba026999a7a434d44afeaddc45817618
em-latic <emanuel.munoz-orlich@outlook.com>
2025-02-26 20:09:39 -0600

    completed exercise project

--- public/client.js
+++ public/client.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
+
+  /*global io*/
+  let socket = io();
+
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
 
+    socket.emit('chat message', messageToSend);
+
     $('#m').val('');
     return false; // prevent form submit from refreshing page
+  });
+
+  socket.on('user count', function(data) {
+    console.log(data);
+  });
+
+  socket.on('user', function (data) {
+    //console.log(data);
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message =
+      data.username +
+      (data.connected ? ' has joined the chat.' : ' has left the chat.');
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+  });
+
+  socket.on('chat message', (data) => {
+    $('#messages').append($('<li>').html('<b>'+ data.username +'</b>' + ': '+ data.message ));
   });
 });
---
+++ routes.js
@@ -0,0 +1,95 @@
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+module.exports = function (app, myDataBase) {
+
+  app.route('/')
+  .get((req, res) => {
+    res.render('index', { 
+      title: 'Connected to Database', 
+      message: 'Please log in',
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true
+    });
+  });
+
+  app.route('/login')
+  .post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) => {
+    res.redirect('/profile');
+  });
+
+  app.route('/register')
+  .post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
+
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: hash //req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+  // 2nd callback for POST req on '/register'
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback')
+  .get(passport.authenticate('github', {failureRedirect: '/'}), (req, res) => {
+    //res.redirect('/profile');
+    req.session.user_id = req.user.id;
+    res.redirect('/chat');
+  });
+
+  app.route('/profile')
+  .get(ensureAuthenticated, (req, res) => {
+    res.render('profile', {username: req.user.username});
+  });
+
+  app.route('/chat')
+  .get(ensureAuthenticated, (req, res) => { 
+    res.render('chat', { user: req.user }) 
+  });
+
+  app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+    .type('text')
+    .send('Not found');
+  })
+}
+
+function ensureAuthenticated(req, res, next) {
+  //console.log('ensureAuthenticated() being run');
+  //console.log(`isAuthenticated = ${req.isAuthenticated()}`);
+
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
